aeb5b6442eb5dfade9eadfb75d1ad3f0b64b0f1c
SaraNoori <sn1044604@student.francistuttle.edu>
2023-10-25 15:12:34 -0500

    okay, NOW im done

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import mypkg.Stack;
 public class App {
     public static void main(String[] args) throws Exception {
         Integer[] t = {1, 2, 3};
-        Stack s = new Stack(t);
+        Stack<Integer> s = new Stack<Integer>(t);
         System.out.println(s);
     }
 }
